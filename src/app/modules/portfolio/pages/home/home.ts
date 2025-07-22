639154dc25f6c2fdd1fe14fa1a9914c1c8a98922
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-07-22 09:28:41 -0300

    feat: experiences

--- src/app/modules/portfolio/pages/home/home.ts
+++ src/app/modules/portfolio/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header";
 import { KnowledgeComponent } from '../../components/knowledge/knowledge';
+import { Experiences } from "../../components/experiences/experiences";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, KnowledgeComponent],
+  imports: [HeaderComponent, KnowledgeComponent, Experiences],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
