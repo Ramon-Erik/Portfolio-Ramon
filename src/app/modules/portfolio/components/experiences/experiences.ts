f800a79d1d751ac145e7b8362f16a48da1c43108
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-07-23 14:43:38 -0300

    fix: removed angular material, using custom

--- src/app/modules/portfolio/components/experiences/experiences.ts
+++ src/app/modules/portfolio/components/experiences/experiences.ts
@@ -1,19 +1,18 @@
 import { Component, signal } from '@angular/core';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-experiences',
   imports: [MatExpansionModule],
   templateUrl: './experiences.html',
-  styleUrl: './experiences.css'
+  styleUrl: './experiences.scss',
 })
 export class ExperiencesCopmponent {
   public experiences = signal([
     {
-      title: "Estágio em Desenvolvimento Web na  Ótica Fênix",
-      description: "Maracanaú | Agosto de 2024 - Dezembro de 2024",
-      text: "<p>Durante meu estágio, atuei na criação de páginas informativas, garantindo acessibilidade e responsividade nos sites da Ótiac Fênix. Utilizei WordPress e JavaScript para desenvolver soluções que melhoraram a experiência do usuário e a presença digital da empresa.</p><p>Além disso, fui responsável pela manutenção e aprimoramento de páginas já existentes, otimizando desempenho e funcionalidades. Essa experiência me permitiu consolidar conhecimentos em desenvolvimento front-end e trabalhar com demandas reais, alinhando técnicas modernas às necessidades do negócio.</p>"
-    }
-  ])
-
+      title: 'Estágio em Desenvolvimento Web na  Ótica Fênix',
+      description: 'Maracanaú | Agosto de 2024 - Dezembro de 2024',
+      text: '<p>Durante meu estágio, atuei na criação de páginas informativas, garantindo acessibilidade e responsividade nos sites da Ótiac Fênix. Utilizei WordPress e JavaScript para desenvolver soluções que melhoraram a experiência do usuário e a presença digital da empresa.</p><p>Além disso, fui responsável pela manutenção e aprimoramento de páginas já existentes, otimizando desempenho e funcionalidades. Essa experiência me permitiu consolidar conhecimentos em desenvolvimento front-end e trabalhar com demandas reais, alinhando técnicas modernas às necessidades do negócio.</p>',
+    },
+  ]);
 }
